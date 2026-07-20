cf9259c5e084a35414bc52a048c8ae1ee4ac9877
ammad-qureshi-dev <ammadq3@gmail.com>
2026-07-19 21:32:39 -0400

    feat: add producer and topic name

--- src/main/java/com/bidder/notification_service/NotificationServiceApplication.java
+++ src/main/java/com/bidder/notification_service/NotificationServiceApplication.java
@@ -2,10 +2,17 @@
 bidder.app */
 package com.bidder.notification_service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
+import static com.bidder.notification_service.utils.Constants.NOTIFICATION_TOPIC;
+
+@Slf4j
 @SpringBootApplication
 @EntityScan("models.entities")
 public class NotificationServiceApplication {
@@ -14,4 +21,11 @@ public class NotificationServiceApplication {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
+	@Bean
+	NewTopic notification() {
+		return TopicBuilder.name(NOTIFICATION_TOPIC)
+				.partitions(1)
+				.replicas(1)
+				.build();
+	}
 }
--- src/main/java/com/bidder/notification_service/utils/Constants.java
+++ src/main/java/com/bidder/notification_service/utils/Constants.java
@@ -12,4 +12,6 @@ public class Constants {
 		public static final String BASE_URI = "/api";
 		public static final String V1 = "/v1";
 	}
+
+	public static final String NOTIFICATION_TOPIC = "notification";
 }
